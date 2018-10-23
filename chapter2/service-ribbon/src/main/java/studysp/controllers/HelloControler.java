671794e69a73b3a7aa0f9873bd621c7d5e935d40
Alex <alexdev@aliyun.com>
2018-10-23 17:58:18 +0800

    add code service-feign

---
+++ chapter2/service-ribbon/src/main/java/studysp/controllers/HelloControler.java
@@ -0,0 +1,22 @@
+package studysp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import studysp.services.HelloService;
+
+/**
+ * Created by alex on 2018/10/23.
+ */
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+}
---
+++ chapter3/service-feign/src/main/java/studysp/inters/SchedualServiceHi.java
@@ -0,0 +1,17 @@
+package studysp.inters;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by alex on 2018/10/23.
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
